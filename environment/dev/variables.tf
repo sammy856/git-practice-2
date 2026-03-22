f1c80811c348e689207edc90da0d03a7df961f84
sammy856 <sameersingh856@gmail.com>
2026-03-22 16:41:56 +0530

    add var type

--- environment/dev/variables.tf
+++ environment/dev/variables.tf
@@ -1,17 +1,41 @@
 variable "resource_groups" {
-  
+  description = "Map of resource groups to create"
+  type = map(object({
+    location = string
+  }))
 }
 
+
 variable "vnets" {
-  
+  description = "Map of VNets with their subnets"
+  type = map(object({
+    resource_group_name = string
+    address_space       = list(string)
+    location            = string
+
+    subnets = list(object({
+      name             = string
+      address_prefixes = list(string)
+    }))
+  }))
 }
+
 
 # variable "linux_vms" {
   
 # }
 
 variable "windowsvm" {
-  
+  description = "Map of windows VMs"
+  type = map(object({
+    resource_group_name  = string
+    virtual_network_name = string
+    location             = string
+    subnet_name          = string
+    vm_size              = string
+    admin_username       = string
+    admin_password       = string
+  }))
 }
 
 variable "nsgs" {
